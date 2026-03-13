ac70c68a6619ea0df327563d7884231242da8a7b
Anna Tarasova <2201236@stud.samgups.ru>
2026-03-13 19:52:27 +0400

    Initial commit: project structure and Django config

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = 'Кинологическая служба ГУ МВД'
+admin.site.site_title = 'Управление системой'
+admin.site.index_title = 'Панель администратора'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('dogs.urls')), 
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
