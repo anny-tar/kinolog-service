ffe0e33506618949b844338615c91694613d7f9c
Anna Tarasova <2201236@stud.samgups.ru>
2026-03-13 19:57:06 +0400

    Rename app verbose name to Кинологическая служба in admin

---
+++ dogs/apps.py
@@ -0,0 +1,9 @@
+# dogs/apps.py
+
+from django.apps import AppConfig
+
+
+class DogsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dogs'
+    verbose_name = 'Кинологическая служба'
